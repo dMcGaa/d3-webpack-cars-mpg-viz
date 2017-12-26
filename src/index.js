d58f3b2571fad3cbd409b4584010dc312e95244d
David McGaa <mcgaa.david@gmail.com>
2017-12-26 11:15:43 -0600

    Added tooltip on hover

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import data from './data/car-data';
 console.log("bundle working");
 console.log(data);
 
+let svg = d3.select('svg#pie g')
 let reduceMpg = (arr) => {
     let acc = {avgMpg: 0, numCars: 1};
     //console.log(arr[0]["model year"]);
@@ -23,12 +24,12 @@ var someData = d3.nest()
 .value((d, i) => reduceMpg(d.values).avgMpg)
     .sort((a, b) => a.key - b.key);
 
-    var tooltip = d3.select("body")
+    //append many divs, only display relevant
+    var tooltip = d3.select('body')
     .append("div")
     .style("position", "absolute")
     .style("z-index", "10")
-    .style("visibility", "hidden")
-    .text("Avg MPG");
+    .style("visibility", "hidden");
 
     let arcData = pieGenerator(someData);
 
@@ -38,7 +39,7 @@ var someData = d3.nest()
 
     console.log(arcData);
 
-    d3.select('svg#pie g')
+    svg 
     .selectAll('path')
     .data(arcData)
 .enter()
@@ -46,14 +47,15 @@ var someData = d3.nest()
     .style('fill', 'orange')
     .style('stroke', 'white')
     .attr('d', arcGenerator)
-    .on("mouseover", function(){
-        return tooltip.style("visibility", "visible");
+    .on("mouseover", function(d) {
+        return tooltip.style("visibility", "visible")
+          .text(() => `year: 19${d.data.key} avg mpg: ${d.value.toFixed(2)}`);
     })
-    .on("mousemove", function(d){
+    .on("mousemove", function(d) {
         return tooltip
-            .style("top", (event.pageY-10)+"px")
-            .style("left",(event.pageX+10)+"px")
-            .text(`Year: ${d.data.key}, MPG: ${d.value.toFixed(2)}`);
+            .style("top", (d3.event.pageY-10) + "px")
+            .style("left", (d3.event.pageX+10) + "px");
+        
     })
     .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
